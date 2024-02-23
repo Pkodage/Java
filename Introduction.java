6c6dd6b1c212d67587f5278a633a184b59af90be
Pkodage <152171889+Pkodage@users.noreply.github.com>
2024-02-23 16:49:15 +0530

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,14 @@
+class Introduction
+{
+    public static void main(String []args)
+    {
+        System.out.println("My name is Poonam Kodage");
+        System.out.println("I am from solapur");
+        System.out.println("I am currently pursuing bechelour degree in computer science engineering from vidya vikas pratishthan solapur");
+        System.out.println("my last sem CGPA was 7.72");
+        System.out.println("My hobbies are drawing,playing badminton,reading books,designing");
+        System.out.println("My strengths are self motivated and having patience in critical situations");
+        System.out.println("My short term goal is getting a job");
+     }
+}
+        
