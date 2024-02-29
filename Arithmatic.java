910351bf7a748c32b83586a8efdb8a98c34f6353
Pkodage <152171889+Pkodage@users.noreply.github.com>
2024-02-29 16:58:04 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,37 @@
+class Arithmatic
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=30;
+		System.out.println("Add="+(a+b));
+
+		int c=50;
+		int d=150;
+		System.out.println("Sub="+(c-d));
+
+		int e=50;
+		int f=10;
+		System.out.println("Mul="+(e*f));
+
+		int g=20;
+		int h=10;
+		System.out.println("Div="+(g/h));
+
+		int i=20;
+		int j=15;
+		System.out.println("Mod="+(i%j));
+
+		float k=20.0f;
+		float l=15.0f;
+		float m=k/l;
+		System.out.println(m);
+	
+		float n=20.0f;
+		float o=15.0f;
+		float p=n%o;
+		System.out.println(p);
+	}
+}
+
+		
