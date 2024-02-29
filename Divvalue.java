70dc60dc5e4de874073ad3b472e72438c6377c0c
Pkodage <152171889+Pkodage@users.noreply.github.com>
2024-02-29 17:02:37 +0530

    Add files via upload

---
+++ Divvalue.java
@@ -0,0 +1,51 @@
+class Divvalue
+{
+	public static void main(String[] args)
+	{
+		
+		byte a=5;
+		byte b=2;
+		int result=a/b;
+		System.out.println(result);
+
+		short c=10;
+		short d=20;
+		int e=c/d;
+
+		char f='a';
+		char g='b';
+		int h=f/g;
+		System.out.println(h);
+
+		int i=100;
+		int j=100;
+		int k=i/j;
+		System.out.println(k);
+		
+		long u=500;
+		long v=300;
+		long w=u/v;
+		System.out.println(w);
+
+		float l=1.1f;
+		float m=2.2f;
+		float n=l/m;
+		System.out.println(n);
+
+		double o=5.5;
+		double p=3.1;
+		double q=o/p;
+		System.out.println(q);
+
+		//boolean x=true;
+		//boolean y=false;
+		//boolean z=x/y;
+		//System.out.println(z);
+		
+		//String r="poonam";
+		//String s="kodage";
+		//String t=r/s;
+		//System.out.println(t);
+
+	}
+}	
