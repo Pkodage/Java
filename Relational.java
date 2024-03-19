d1a74fa41b21ba8efd5698b4dcfc2ad95c3deda1
Pkodage <152171889+Pkodage@users.noreply.github.com>
2024-03-19 17:11:58 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,30 @@
+class Relational
+{
+	public static void main(String[] args)
+	{
+		System.out.println(3<4);
+		System.out.println('a'>'A');
+
+		System.out.println(0.0>0);
+		System.out.println('q'<=113);
+		
+		//System.out.println(true>false);
+		System.out.println(59.9999==60);
+
+		System.out.println(true==false);
+		System.out.println(true!=true);
+		
+		//System.out.println(byte<short);
+		//System.out.println(true==1);
+		
+		//System.out.println("hello"<"hi");
+		System.out.println("hi"!="bye");
+		System.out.println("Hi"=="hi");
+
+		System.out.println(1.1f<1.1);
+		System.out.println((22.0/7)>(25.0f/7.0f));
+		System.out.println(22.0f/7.0f);
+
+	}
+}
+		
