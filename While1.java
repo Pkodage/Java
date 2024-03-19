345268763aa30e30d17e8ad31ff6f74c2c967510
Pkodage <152171889+Pkodage@users.noreply.github.com>
2024-03-19 17:00:54 +0530

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class While1
+{
+	public static void main(String[] args)
+	{
+		int count=0;
+		while(count<10)
+		{
+			System.out.println("have a great day!!");
+			++count;
+		}
+	}
+}
